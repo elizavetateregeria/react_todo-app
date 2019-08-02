1a6b7fc0875d977a528bdff5527b11ec60d6de2c
felsi13 <13felsi@gmail.com>
2019-08-02 17:00:39 +0300

    Updates

--- precache-manifest.8200cc0eb2c6ddcb27e20f3046887021.js
+++ precache-manifest.8200cc0eb2c6ddcb27e20f3046887021.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "21dbdc96efa75911fd43872003328225",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "140b615b4d2cd502ab25",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "2249d86949b54dc0e89e",
+    "url": "/react_todo-app/static/js/2.09aed800.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "140b615b4d2cd502ab25",
+    "url": "/react_todo-app/static/js/main.d0b0cb5e.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
